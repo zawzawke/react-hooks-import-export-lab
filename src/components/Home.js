59d7744456d362afc094396ca2e2fc4c069057f4
Paul <paulallanmain@gmail.com>
2025-04-11 11:14:36 +0300

    Complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {username, city} from "../data/user"
+
+console.log(username, city)
 
 function Home() {
   return (
@@ -9,3 +12,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
